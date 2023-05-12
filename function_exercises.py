cdf67f2f69bac883b484c3b9f8846c6f7b8b1409
mannievilla <mannievilla@gmail.com>
2023-05-12 15:45:34 -0500

    saving

---
+++ function_exercises.py
@@ -0,0 +1,10 @@
+# Define a function named apply_discount. 
+# It should accept a original price, 
+# and a discount percentage, and return the 
+# price after the discount is applied.
+
+
+def apply_discounts(x):
+    return 5 * x
+
+print(apply_discounts(10))
